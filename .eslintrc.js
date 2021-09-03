166717ec08c22629c941178902dc83c2afde0b73
Furkan TAN <tanfurkan@gmail.com>
2021-09-03 12:36:51 +0300

    Eslint Prettier Config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
 		es2021: true,
 		node: true,
 	},
-	extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+	extends: ['eslint:recommended', 'react-app', 'prettier'],
 	parserOptions: {
 		ecmaFeatures: {
 			jsx: true,
