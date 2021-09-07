d5cd1814f03c27b751a59e0dd67c6ff0190e256b
Furkan TAN <tanfurkan@gmail.com>
2021-09-07 23:54:41 +0300

    Theme color change

--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,11 @@
 import { createTheme } from '@material-ui/core/styles';
-import deepOrange from '@material-ui/core/colors/deepOrange';
+import { deepOrange, orange } from '@material-ui/core/colors';
 
 // A custom orange theme for this app
 const theme = createTheme({
 	palette: {
 		primary: deepOrange,
-		secondary: {
-			main: '#ef5350',
-		},
+		secondary: orange,
 	},
 });
 
