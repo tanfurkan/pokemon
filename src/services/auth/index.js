37a035783d724585e591a934450e0f9f683e04fe
Furkan TAN <tanfurkan@gmail.com>
2021-09-08 13:21:43 +0300

    Prettier Fix

--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_LIVE_URL : process.env.REACT_APP_DEV_URL;
+const baseURL =
+	process.env.NODE_ENV === 'production'
+		? process.env.REACT_APP_LIVE_URL
+		: process.env.REACT_APP_DEV_URL;
 
 const authAxios = axios.create({
 	baseURL,
